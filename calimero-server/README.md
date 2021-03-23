fbc9b24c779a2ba199a1b24425be425ad2cbb022
aschamberger <mail@andreass.net>
2021-03-23 22:30:59 +0100

    Add files via upload

--- calimero-server/README.md
+++ calimero-server/README.md
@@ -1,1 +1,63 @@
+# Home Assistant Add-On: Calimero Server
 
+A KNXnet/IP server for running your own KNXnet/IP server in software. This add-on can be used for accessing the KNX bus via FT1.2 or TPUART serial adapters. 
+
+The minimum required runtime environment is Java SE 11 (java.base) and a custom Java runtime is created via jlink. Java 11 is only available for AArch64 on ARM.
+
+## Add-On Configuration
+
+The add-on requires the following configuration:
+```
+knx_address: 1.1.0
+client_address_start: 1.1.101
+client_address_count: 5
+interface_type: ft12-cemi
+device: /dev/ttyAMA0
+routing: true
+loglevel: info
+```
+
+`knx_address` of the service container, will be visible in e.g. ETS-tool. If routing is activated, requires a coupler/backbone address (x.y.0 or x.0.0).
+
+`client_address_start` Start address of KNX individual addresses, which are assigned to KNXnet/IP tunneling connections.
+
+`client_address_count` number of KNX individual addresses/possible tunneling connections.
+
+`interface_type` can be one of:
+* `ft12-cemi` for e.g. the [KNX BAOS Module 838 kBerry](https://www.weinzierl.de/index.php/en/all-knx/knx-module-en/knx-baos-module-838-en) 
+* `tpuart` for e.g. [TPUART USB Modul](http://shop.busware.de/product_info.php/products_id/59)
+
+`device` uart device name
+
+`routing` if true activate KNX IP routing, if false routing is disabled
+
+`loglevel` is optional and can be one of the slf4j log levels. It logs everything to standard output. 
+
+## Configure serial communication 
+
+### Raspberry Pi 3 / 4
+ 
+In order to use the KNX BAOS Module 838 kBerry the serial port on the Raspberry Pi needs to be enabled in `config.txt` (which is by default occupied by the bluetooth module).
+
+Raspberry Pi 3
+```
+dtoverlay=pi3-disable-bt
+```
+
+[Raspberry Pi 4](https://github.com/knxd/knxd/issues/469#issuecomment-723936998)
+
+```
+dtoverlay=disable-bt
+dtoverlay=uart0
+[all]
+uart0=on
+```
+
+### ODROID C4 / Asus Tinker Board S
+
+The correct device for the serial port is '/dev/ttyS1' (the device is pin compatible to the Raspberry Pi)
+
+## Links
+
+* Enable SSH access to the host: https://developers.home-assistant.io/docs/operating-system/debugging/
+* USB could be added too, however usb4java also needs to be compiled for musl on Alpine: http://usb4java.org/nativelibs.html
---
+++ calimero-server/run.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+set -e
+
+CONFIG_PATH=/data/options.json
+
+KNX_ADDRESS=$(jq --raw-output ".knx_address" $CONFIG_PATH)
+CLIENT_ADDRESS_START=$(jq --raw-output ".client_address_start" $CONFIG_PATH)
+CLIENT_ADDRESS_COUNT=$(jq --raw-output ".client_address_count" $CONFIG_PATH)
+INTERFACE_TYPE=$(jq --raw-output ".interface_type" $CONFIG_PATH)
+DEVICE=$(jq --raw-output ".device" $CONFIG_PATH)
+ROUTING=$(jq --raw-output ".routing" $CONFIG_PATH)
+LOGLEVEL=$(jq --raw-output ".loglevel" $CONFIG_PATH)
+
+ADD_LOGGING=""
+if [ -n "$LOGLEVEL" ]; then
+    ADD_LOGGING=" -Dorg.slf4j.simpleLogger.defaultLogLevel=$LOGLEVEL"
+fi
+
+CONFIG_XML="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!-- Calimero server settings (required for startup) -->
+<knxServer name=\"knx-server\" friendlyName=\"Calimero KNXnet/IP Server (on Home Assistant OS)\">
+	<!-- KNXnet/IP search & discovery -->
+	<discovery listenNetIf=\"all\" outgoingNetIf=\"all\" activate=\"true\" />
+	<!-- Provides the KNXnet/IP-side configuration for access to one KNX subnet -->
+	<serviceContainer activate=\"true\" routing=\"$ROUTING\" networkMonitoring=\"true\" 
+		udpPort=\"3671\" listenNetIf=\"any\">
+		<knxAddress type=\"individual\">$KNX_ADDRESS</knxAddress>"
+if [ "$INTERFACE_TYPE" = "ft12-cemi" ]; then
+CONFIG_XML="$CONFIG_XML
+		<knxSubnet type=\"ft12-cemi\" medium=\"tp1\">$DEVICE</knxSubnet>"
+fi
+if [ "$INTERFACE_TYPE" = "tpuart" ]; then
+CONFIG_XML="$CONFIG_XML
+		<knxSubnet type=\"tpuart\" medium=\"tp1\">$DEVICE</knxSubnet>"
+fi
+CONFIG_XML="$CONFIG_XML
+		<!-- KNX group address filter applied by the server for this service container (optional) -->
+		<groupAddressFilter>
+		</groupAddressFilter>
+		<!-- Additional KNX individual addresses assigned to client KNXnet/IP connections (optional) -->
+		<additionalAddresses>"
+export KNX_ADDRESS_PREFIX=$(echo $CLIENT_ADDRESS_START|cut -d'.' -f 1-2)
+export START_OCTET=$(echo $CLIENT_ADDRESS_START|cut -d'.' -f 3)
+# Add new KNX Client Address elements
+i=1
+while [ "$i" -le $CLIENT_ADDRESS_COUNT ]; do
+CURRENT_OCTET=$(expr $START_OCTET + $i - 1)
+CONFIG_XML="$CONFIG_XML
+			<knxAddress type=\"individual\">$KNX_ADDRESS_PREFIX.$CURRENT_OCTET</knxAddress>"
+i=$(( i + 1 ))
+done
+CONFIG_XML="$CONFIG_XML
+		</additionalAddresses>
+	</serviceContainer>
+	<!-- Add next service container (optional) -->
+</knxServer>"
+
+echo "$CONFIG_XML" > /etc/server-config.xml
+
+cat /etc/server-config.xml
+
+exec /opt/jdk/bin/java -cp "/opt/calimero/*"$ADD_LOGGING tuwien.auto.calimero.server.Launcher /etc/server-config.xml
