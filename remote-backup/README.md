283473a965d6ad2c8c46ee94d92dbe58c655171c
aschamberger <mail@andreass.net>
2021-09-29 14:38:51 +0200

    Add files via upload

--- remote-backup/README.md
+++ remote-backup/README.md
@@ -1,1 +1,49 @@
+# Remote Backup Hass.io Add-On
 
+Automatically create Hass.io snapshots to remote server location using SCP.
+
+When the add-on is started the following happens:
+1. Snapshot is created locally with a timestamp name, e.g. `Automated backup 2018-03-04 04:00`.
+1. It is copied to the specified remote location using SCP.
+1. The local backup is removed again (optional).
+
+Based on https://github.com/rccoleman/hassio-remote-backup / https://github.com/overkill32/hassio-remote-backup
+
+## Add-On Configuration
+
+The add-on requires the following configuration:
+```json
+{
+  "ssh_host": "192.168.1.2",
+  "ssh_port": 22,
+  "ssh_user": "root",
+  "ssh_key": [
+"-----BEGIN RSA PRIVATE KEY-----",
+"MIICXAIBAAKBgQDTkdD4ya/Qxz5xKaKojVIOVWjyeyEoEuAafAvYvppqmaBhyh4N",
+"5av4i87y8tdGusdq7V0Zj0+js4jEdvJRDrXJBrp1neLfsjkF6t1XLfrA51Ll9SXF",
+"...",
+"X+6r/gTvUEQv1ufAuUE5wKcq9FsbnTa3FOF0PdQDWl0=",
+"-----END RSA PRIVATE KEY-----"
+  ],
+  "remote_directory": "~/hassio-backups",
+  "password": "password_protect_it",
+  "keep_local_backup": "14"
+}
+```
+
+`password` and `keep_local_backup` are optional attributes. `keep_local_backup` controls how many local backups you want to preserve. Default (`""`) is to keep no local backups created from this addon. If `all` then all local backups will be preserved. A positive integer will determine how many of the latest backups will be preserved. Note this will delete other local backups created outside this addon.
+	
+## Example automating daily backups
+
+_configuration.yaml_
+```yaml
+automations:
+  - alias: Daily Backup at 4 AM
+  trigger:
+    platform: time
+    at: '4:00:00'
+  action:
+  - service: hassio.addon_start
+    data:
+      addon: local_remote_backup
+```
---
+++ remote-backup/run.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+HA=/usr/bin/ha
+
+# parse inputs from options
+SSH_HOST=$(jq --raw-output ".ssh_host" $CONFIG_PATH)
+SSH_PORT=$(jq --raw-output ".ssh_port" $CONFIG_PATH)
+SSH_USER=$(jq --raw-output ".ssh_user" $CONFIG_PATH)
+SSH_KEY=$(jq --raw-output ".ssh_key[]" $CONFIG_PATH)
+REMOTE_DIRECTORY=$(jq --raw-output ".remote_directory" $CONFIG_PATH)
+PASSWORD=$(jq --raw-output '.password' $CONFIG_PATH)
+KEEP_LOCAL_BACKUP=$(jq --raw-output '.keep_local_backup' $CONFIG_PATH)
+
+# create variables
+SSH_ID="${HOME}/.ssh/id"
+
+function add-ssh-key {
+    echo "Adding SSH key"
+    mkdir -p ~/.ssh
+    (
+        echo "Host remote"
+        echo "    IdentityFile ${HOME}/.ssh/id"
+        echo "    HostName ${SSH_HOST}"
+        echo "    User ${SSH_USER}"
+        echo "    Port ${SSH_PORT}"
+        echo "    StrictHostKeyChecking no"
+    ) > "${HOME}/.ssh/config"
+
+    while read -r line; do
+        echo "$line" >> ${HOME}/.ssh/id
+    done <<< "$SSH_KEY"
+
+    chmod 600 "${HOME}/.ssh/config"
+    chmod 600 "${HOME}/.ssh/id"
+}
+
+function copy-backup-to-remote {
+
+    cd /backup/
+    echo "Copying ${slug}.tar to ${REMOTE_DIRECTORY} on ${SSH_HOST} using SCP"
+    scp -F "${HOME}/.ssh/config" "${slug}.tar" remote:"${REMOTE_DIRECTORY}"
+}
+
+function delete-local-backup {
+
+    ${HA} snapshots reload
+
+    if [[ ${KEEP_LOCAL_BACKUP} == "all" ]]; then
+        :
+    elif [[ -z ${KEEP_LOCAL_BACKUP} ]]; then
+        echo "Deleting local backup: ${slug}"
+        ${HA} snapshots remove ${slug}
+    else
+
+        last_date_to_keep=$($HA --raw-json snapshots list | jq .data.snapshots[].date | sort -r | \
+            head -n "${KEEP_LOCAL_BACKUP}" | tail -n 1 | xargs date -D "%Y-%m-%dT%T" +%s --date )
+
+        $HA --raw-json snapshots list | jq -c .data.snapshots[] | while read backup; do
+            if [[ $(echo ${backup} | jq .date | xargs date -D "%Y-%m-%dT%T" +%s --date ) -lt ${last_date_to_keep} ]]; then
+                echo "Deleting local backup: $(echo ${backup} | jq -r .slug)"
+                ${HA} snapshots remove $(echo ${backup} | jq -r .slug)
+            fi
+        done
+
+    fi
+    echo Exiting
+}
+
+function create-local-backup {
+    name="Automated backup $(date +'%Y-%m-%d %H:%M')"
+    echo "Creating local backup: \"${name}\""
+	if [[ -z $PASSWORD  ]]; then
+      slug=$(${HA} snapshots new --name="${name}" | cut -d' ' -f2)
+	else
+      slug=$(${HA} snapshots new --name="${name}" --password="${PASSWORD}" | cut -d' ' -f2)
+	fi
+    echo "Backup created: ${slug}"
+}
+
+
+add-ssh-key
+create-local-backup
+copy-backup-to-remote
+delete-local-backup
+
+echo "Backup process done!"
+exit 0
