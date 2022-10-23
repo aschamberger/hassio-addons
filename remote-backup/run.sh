2ea88034b239b2211e34fc5abc1b51210efeb0b5
aschamberger <mail@andreass.net>
2022-10-23 20:19:47 +0200

    Update remote backup

--- remote-backup/run.sh
+++ remote-backup/run.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-set -e
+#!/usr/bin/with-contenv bash
 
 CONFIG_PATH=/data/options.json
-HA=/usr/bin/ha
 
 # parse inputs from options
 SSH_HOST=$(jq --raw-output ".ssh_host" $CONFIG_PATH)
@@ -13,54 +11,51 @@ REMOTE_DIRECTORY=$(jq --raw-output ".remote_directory" $CONFIG_PATH)
 PASSWORD=$(jq --raw-output '.password' $CONFIG_PATH)
 KEEP_LOCAL_BACKUP=$(jq --raw-output '.keep_local_backup' $CONFIG_PATH)
 
-# create variables
-SSH_ID="${HOME}/.ssh/id"
-
 function add-ssh-key {
     echo "Adding SSH key"
-    mkdir -p ~/.ssh
+    mkdir -p /.ssh
     (
         echo "Host remote"
-        echo "    IdentityFile ${HOME}/.ssh/id"
+        echo "    IdentityFile /.ssh/id"
         echo "    HostName ${SSH_HOST}"
         echo "    User ${SSH_USER}"
         echo "    Port ${SSH_PORT}"
         echo "    StrictHostKeyChecking no"
-    ) > "${HOME}/.ssh/config"
+    ) > "/.ssh/config"
 
     while read -r line; do
-        echo "$line" >> ${HOME}/.ssh/id
+        echo "$line" >> /.ssh/id
     done <<< "$SSH_KEY"
 
-    chmod 600 "${HOME}/.ssh/config"
-    chmod 600 "${HOME}/.ssh/id"
+    chmod 600 "/.ssh/config"
+    chmod 600 "/.ssh/id"
 }
 
 function copy-backup-to-remote {
 
     cd /backup/
     echo "Copying ${slug}.tar to ${REMOTE_DIRECTORY} on ${SSH_HOST} using SCP"
-    scp -F "${HOME}/.ssh/config" "${slug}.tar" remote:"${REMOTE_DIRECTORY}"
+    scp -F "/.ssh/config" "${slug}.tar" remote:"${REMOTE_DIRECTORY}"
 }
 
 function delete-local-backup {
 
-    ${HA} backups reload
+    ha backups reload
 
     if [[ ${KEEP_LOCAL_BACKUP} == "all" ]]; then
         :
     elif [[ -z ${KEEP_LOCAL_BACKUP} ]]; then
         echo "Deleting local backup: ${slug}"
-        ${HA} backups remove ${slug}
+        ha backups remove ${slug}
     else
 
-        last_date_to_keep=$($HA --raw-json backups list | jq .data.snapshots[].date | sort -r | \
+        last_date_to_keep=$($HA --raw-json backups list | jq .data.backups[].date | sort -r | \
             head -n "${KEEP_LOCAL_BACKUP}" | tail -n 1 | xargs date -D "%Y-%m-%dT%T" +%s --date )
 
-        $HA --raw-json backups list | jq -c .data.snapshots[] | while read backup; do
+        ha --raw-json backups list | jq -c .data.backups[] | while read backup; do
             if [[ $(echo ${backup} | jq .date | xargs date -D "%Y-%m-%dT%T" +%s --date ) -lt ${last_date_to_keep} ]]; then
                 echo "Deleting local backup: $(echo ${backup} | jq -r .slug)"
-                ${HA} backups remove $(echo ${backup} | jq -r .slug)
+                ha backups remove $(echo ${backup} | jq -r .slug)
             fi
         done
 
@@ -72,13 +67,12 @@ function create-local-backup {
     name="Automated backup $(date +'%Y-%m-%d %H:%M')"
     echo "Creating local backup: \"${name}\""
 	if [[ -z $PASSWORD ]]; then
-      slug=$(${HA} backups new --name="${name}" | cut -d' ' -f2)
+      slug=$(ha backups new --name="${name}" | cut -d' ' -f2)
 	else
-      slug=$(${HA} backups new --name="${name}" --password="${PASSWORD}" | cut -d' ' -f2)
+      slug=$(ha backups new --name="${name}" --password="${PASSWORD}" | cut -d' ' -f2)
 	fi
     echo "Backup created: ${slug}"
 }
-
 
 add-ssh-key
 create-local-backup
