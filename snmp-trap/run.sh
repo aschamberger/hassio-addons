e624c5243f01d80b871282825567e756bb6082eb
georgegougoudis <156668518+georgegougoudis@users.noreply.github.com>
2024-11-30 10:33:30 +0200

    Delete snmp-trap directory

--- snmp-trap/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bash
-
-CONFIG_PATH=/data/options.json
-
-export PORT=$(jq --raw-output ".port" $CONFIG_PATH)
-export COMMUNITY_STRING=$(jq --raw-output ".community_string" $CONFIG_PATH)
-export PYTHONUNBUFFERED=1
-
-exec python3 trap.py
